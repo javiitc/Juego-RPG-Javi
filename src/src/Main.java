92579dab0152eba8b46eb0f31dec08da8d37556f
Javi <itsjavii1610@gmail.com>
2025-11-28 11:29:11 +0100

    Creacion de personajes

--- src/src/Main.java
+++ src/src/Main.java
@@ -6,6 +6,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
-        
+        System.out.println("=== Bienvenido al mundo de Jujutsu Kaisen ===. \n" +
+                "Este mundo esta lleno de maldiciones que nacen de las malas emociones de las personas. Tu deber como hechicero es derrotar a todas las maldiciones posibles y no morir en el intento, \n" +
+                "pero cuidado, hay maldiciones muy fuertes que solo un hechicero de primer grado o de grado especial podría hacerles frente. \n" +
+                "Derrota a las que puedas, y espero que no te encuentres con el rey de las maldiciones");
+        System.out.println("Ahora elige un hechicero para continuar (Ten en cuenta que cada uno tiene caracteristicas y ataques unicos)");
+
+
     }
 }
---
+++ src/src/Personajes.java
@@ -0,0 +1,5 @@
+public class Personajes {
+    public static void main(String[] args) {
+
+    }
+}
