23d1a7b494688cc55dde9655a54589a5b00854fe
Javi <itsjavii1610@gmail.com>
2025-11-28 11:52:47 +0100

    Stats

--- src/src/Personajes.java
+++ src/src/Personajes.java
@@ -2,7 +2,12 @@ public class Personajes {
     public static void main(String[] args) {
         int [] vidaPersonajes = {150, 100, 120, 180};
         String [] expansionesDominio = {"Vacío Infinito", "Amor mutuo y verdadero", "Golpe al Alma",  "Lanza del Cielo Invertida"};
+
         int energiaMaldita = 0;
-        
+        double multiplicador = 0.0;
+        int dmgAtaqueFisico = 0;
+        int yenes = 0;
+        int dmgAtaqueEspecial = 0;
+
     }
 }
