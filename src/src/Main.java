04fe1063b12152b3637f2516c9a55e63f3515887
Javi <itsjavii1610@gmail.com>
2025-11-28 11:50:16 +0100

    Personajes

--- src/src/Main.java
+++ src/src/Main.java
@@ -9,9 +9,11 @@ public class Main {
         System.out.println("=== Bienvenido al mundo de Jujutsu Kaisen ===. \n" +
                 "Este mundo esta lleno de maldiciones que nacen de las malas emociones de las personas. Tu deber como hechicero es derrotar a todas las maldiciones posibles y no morir en el intento, \n" +
                 "pero cuidado, hay maldiciones muy fuertes que solo un hechicero de primer grado o de grado especial podría hacerles frente. \n" +
-                "Derrota a las que puedas, y espero que no te encuentres con el rey de las maldiciones");
+                "Derrota a las que puedas, y espero que no te encuentres con el rey de las maldiciones, Sukuna");
         System.out.println("Ahora elige un hechicero para continuar (Ten en cuenta que cada uno tiene caracteristicas y ataques unicos)");
-
-
+        System.out.println("1. Satoru Gojo \n" +
+                "2. Yuta Okkotusu \n" +
+                "3. Yuji Itadori \n" +
+                "4. Toji Fushiguro");
     }
 }
--- src/src/Personajes.java
+++ src/src/Personajes.java
@@ -1,5 +1,8 @@
 public class Personajes {
     public static void main(String[] args) {
-
+        int [] vidaPersonajes = {150, 100, 120, 180};
+        String [] expansionesDominio = {"Vacío Infinito", "Amor mutuo y verdadero", "Golpe al Alma",  "Lanza del Cielo Invertida"};
+        int energiaMaldita = 0;
+        
     }
 }
