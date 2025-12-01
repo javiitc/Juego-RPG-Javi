7a7c51e475b4b4510bfe7c3d8c0b22b2d37eda7d
Javi <itsjavii1610@gmail.com>
2025-12-01 10:17:47 +0100

    Variable arreglada

--- src/src/Main.java
+++ src/src/Main.java
@@ -7,28 +7,44 @@ public class Main {
         Random random = new Random();
 
         int seleccionPersonaje;
+        boolean confirmacion = true;
+        int confirmar ;
+        Personajes pnjSeleccionado = new Personajes();
+
 
         System.out.println("=== Bienvenido al mundo de Jujutsu Kaisen ===. \n" +
                 "Este mundo esta lleno de maldiciones que nacen de las malas emociones de las personas. Tu deber como hechicero es derrotar a todas las maldiciones posibles y no morir en el intento, \n" +
                 "pero cuidado, hay maldiciones muy fuertes que solo un hechicero de primer grado o de grado especial podría hacerles frente. \n" +
                 "Derrota a las que puedas, y espero que no te encuentres con el rey de las maldiciones, Sukuna");
-        System.out.println("Ahora elige un hechicero para continuar (Ten en cuenta que cada uno tiene caracteristicas y ataques unicos)");
-        System.out.println("1. Satoru Gojo \n" +
-                "2. Yuta Okkotusu \n" +
-                "3. Yuji Itadori \n" +
-                "4. Toji Fushiguro");
 
-        seleccionPersonaje = sc.nextInt();
+        while (confirmacion) {
+            System.out.println("Ahora elige un hechicero para continuar (Ten en cuenta que cada uno tiene caracteristicas y ataques unicos)");
+            System.out.println("1. Satoru Gojo \n" +
+                    "2. Yuta Okkotusu \n" +
+                    "3. Yuji Itadori \n" +
+                    "4. Toji Fushiguro");
 
-        Personajes pnjSeleccionado = new Personajes();
+            seleccionPersonaje = sc.nextInt();
 
-        pnjSeleccionado.check_PJ(seleccionPersonaje);
+            pnjSeleccionado.check_PJ(seleccionPersonaje);
 
-        System.out.println( pnjSeleccionado.descripcionPersonaje + "\n" +
-                           "Vida máxima: " + pnjSeleccionado.vidaPersonajes + "\n" +
-                           "Energia Maldita: " + pnjSeleccionado.energiaMaldita + "\n" +
-                           "Daño Ataque fisico: " + pnjSeleccionado.dmgAtaqueFisico + "\n" +
-                           "Ataque especial: " + pnjSeleccionado.tecnicaEspecial + " | Daño: " + pnjSeleccionado.dmgAtaqueEspecial + "\n" +
-                           "Expansión de Dominio: " + pnjSeleccionado.expansionesDominio + " | Daño: " + pnjSeleccionado.dmgExpansionDominio);
+            System.out.println( pnjSeleccionado.descripcionPersonaje + "\n" +
+                    "Vida máxima: " + pnjSeleccionado.vidaPersonajes + "\n" +
+                    "Energia Maldita: " + pnjSeleccionado.energiaMaldita + "\n" +
+                    "Daño Ataque fisico: " + pnjSeleccionado.dmgAtaqueFisico + "\n" +
+                    "Ataque especial: " + pnjSeleccionado.tecnicaEspecial + " | Daño: " + pnjSeleccionado.dmgAtaqueEspecial + "\n" +
+                    "Expansión de Dominio: " + pnjSeleccionado.expansionesDominio + " | Daño: " + pnjSeleccionado.dmgExpansionDominio + "\n");
+
+            System.out.println("Confirmar selección?");
+            System.out.println("1. SI | 2. NO");
+
+            confirmar = sc.nextInt();
+
+            if (confirmar == 1) {
+                confirmacion = false;
+            }
+        }
+
+        System.out.println(pnjSeleccionado.descripcionPersonaje);
     }
 }
