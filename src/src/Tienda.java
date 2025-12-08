3bc965de0a54a69e240b72c2a4926f7699c2021c
Javi <itsjavii1610@gmail.com>
2025-12-08 18:53:00 +0100

    Tienda

--- src/src/Tienda.java
+++ src/src/Tienda.java
@@ -5,9 +5,27 @@ public class Tienda {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("==== TIENDA DE OBJETOS ====");
-        System.out.println("1. " + objetos [0] + ": ");
-        System.out.println("1. " + objetos [1] + ": ");
-        System.out.println("1. " + objetos [2] + ": ");
-        System.out.println("1. " + objetos [3] + ": ");
+        System.out.println("1. " + objetos [0] + ": 3 yenes ");
+        System.out.println("2. " + objetos [1] + ": 5 yenes ");
+        System.out.println("3. " + objetos [2] + ": 10 yenes ");
+        System.out.println("4. " + objetos [3] + ": 3 yenes ");
+        System.out.println("5. Salir");
+
+        int seleccionTienda = sc.nextInt();
+
+        if (seleccionTienda == 1) {
+            System.out.println("Has comprado: " + objetos [0]);
+            personaje.yenes -= 3;
+            personaje.inventario
+            System.out.println("Yenes: " + personaje.yenes);
+        } else if (seleccionTienda == 2) {
+            System.out.println("Has comprado: " + objetos [1]);
+        } else if (seleccionTienda == 3) {
+            System.out.println();
+        } else if (seleccionTienda == 4) {
+            System.out.println();
+        } else {
+            
+        }
     }
 }
